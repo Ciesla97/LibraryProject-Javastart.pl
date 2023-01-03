5a13cdce572f04141d587fe997bd5d158567032a
jakubciesla <jakubciesla0@gmail.com>
2023-01-03 19:21:49 +0100

    Commit

---
+++ pl/javastart/library/exception/PubliactionsAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package pl.javastart.library.exception;
+
+public class PubliactionsAlreadyExistsException extends RuntimeException{
+    public PubliactionsAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ pl/javastart/library/model/Library.java
@@ -0,0 +1,64 @@
+package pl.javastart.library.model;
+
+import pl.javastart.library.exception.PubliactionsAlreadyExistsException;
+import pl.javastart.library.exception.UserAlreadyExistsException;
+
+import java.io.Serializable;
+import java.util.*;
+
+public class Library implements Serializable {
+
+    private Map<String, Publication> publications = new HashMap<>();
+    private Map<String, LibraryUser> users  = new HashMap<>();
+
+    public Map<String, Publication> getPublications() {
+        return publications;
+    }
+
+    public Collection<Publication> getSortedPublications(Comparator<Publication> comparator) {
+        List<Publication> list = new ArrayList<>(publications.values());
+        list.sort(comparator);
+        return list;
+    }
+    public Optional<Publication> findPublicationByTitle(String title) {
+        return Optional.ofNullable(publications.get(title));
+    }
+
+    public Map<String, LibraryUser> getUsers() {
+        return users;
+    }
+
+    public Collection<LibraryUser> getSortedUsers(Comparator<LibraryUser>comparator) {
+        List<LibraryUser> list = new ArrayList<>(users.values());
+        list.sort(comparator);
+        return list;
+    }
+
+    public void addPublication(Publication publication){
+        if(publications.containsKey(publication.getTitle())) {
+            throw new PubliactionsAlreadyExistsException(
+                    "Publikacja o takim tytule już istnieje"
+            );
+        }
+        publications.put(publication.getTitle(),publication);
+        }
+    public void addUser(LibraryUser user){
+        if(users.containsKey(user.getPesel())) {
+            throw new UserAlreadyExistsException(
+                    "Użytkownik ze wskazanym peselem już istnieje" + user.getPesel()
+            );
+        }
+        users.put(user.getPesel(),user);
+    }
+
+        public boolean removePublication(Publication pub) {
+            if (publications.containsValue(pub)) {
+                publications.remove(pub.getTitle());
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
